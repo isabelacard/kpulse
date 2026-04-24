9ba2d5ef8d89b61760be43cbee1237a343025694
samuel florez <samuel.f.v.t.p@gmail.com>
2026-04-24 10:34:46 -0500

    feat: add mobile pages of the instructions

---
+++ screen/src/pages/instructions/Mobile/CalibratingMobile.tsx
@@ -0,0 +1,18 @@
+import ImagenFondoCelular from "../../../assets/fondocelularinstructions.png";
+
+function CalibratingMobile() {
+    return (
+        <div className="flex items-center justify-center w-full h-screen p-4 overflow-hidden">
+            <div className="relative flex items-center justify-center">
+                <img className="max-w-full max-h-full object-contain" src={ImagenFondoCelular} />
+                <div className="absolute z-10 flex flex-col items-center h-full top-80">
+                    <div className="flex flex-col mt-10">
+                        <h1 className="text-white text-4xl text-center">Calibrating...</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CalibratingMobile;
