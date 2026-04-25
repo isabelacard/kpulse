b3283199505da5c24ae958987a86f45732bbb33f
Mariana <1110366789@u.icesi.edu.co>
2026-04-25 16:47:17 -0500

    feat: Add new visual complementary elements

--- screen/src/pages/onboarding/components/InfoCard.tsx
+++ screen/src/pages/onboarding/components/InfoCard.tsx
@@ -15,8 +15,8 @@ export const InfoCard = ({ icon, title, description}: InfoCardProps) => {
             </div>
             {/*Lado de texto*/}
             <div className="flex flex-col justify-center px-6 py-2 text-white">
-                <h3 className="font-bold text-lg leading-tight">{title}</h3>
-                <p className="text-white/80 text-[10px] leading-snug mt-1">{description}</p>
+                <h3 className="font-medium text-[18px] leading-tight font-poppins">{title}</h3>
+                <p className="text-white/80 text-[10.5px] leading-snug mt-1 font-poppins font-light">{description}</p>
             </div>
         </div>
     );
--- screen/src/pages/onboarding/onboarding.tsx
+++ screen/src/pages/onboarding/onboarding.tsx
@@ -1,5 +1,7 @@
 import { InfoCard } from "./components/InfoCard";
 import { Qrcode } from "./components/QrCode";
+import { HelpCircle, Target, Trophy } from "lucide-react";
+
 import imagendefondo from "../../assets/imagendefondo.png";
 import circuloamarillo from "../../assets/circuloamarillo.png";
 import grupodecorativo2 from "../../assets/grupodecorativo2.png";
@@ -17,7 +19,7 @@ export default function Onboarding() {
                     backgroundPosition: "center",
                 }}
             >
-                {/* --- CAPA DE FONDO DECORATIVA (Z-Index 0) --- */}
+
                 <img 
                     src={circuloamarillo} 
                     alt="Decoración" 
@@ -26,18 +28,31 @@ export default function Onboarding() {
                 <img
                     src={granny}
                     alt="abuelita"
-                    className="absolute bottom-0 right-75 h-[92%] z-0"
+                    className="absolute bottom-0 right-70 h-[92%] z-0"
                 />
 
-                {/* --- CAPA DE CONTENIDO PRINCIPAL --- */}
-                {/* Agregamos justify-between para empujar el QR a la derecha */}
+                {/* --- FORMA DEL TOPE SUPERIOR (El "Notch" recortado) --- */}
+                {/* Lo centramos con left-1/2 y -translate-x-1/2, pegado al top-0 */}
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-91.5px h-[42px]">
+                    <svg 
+                        viewBox="0 0 366 59" 
+                        fill="none" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        className="w-full h-full"
+                    >
+                        <path 
+                            d="M0 0 C 45 0, 65 59, 110 59 L 256 59 C 301 59, 321 0, 366 0 Z" 
+                            fill="white"
+                        />
+                    </svg>
+                </div>
+
                 <div className="relative z-10 flex flex-col lg:flex-row items-center justify-between gap-12 w-full max-w-7xl ml-7 pr-10">
                     
-                    {/* 1. Textos e InfoCards (Ahora a la Izquierda) */}
                     <div className="max-w-2xl relative z-10">
                         <header className="mb-10">
                             <h1 className="text-white text-[90px] font-bold leading-[1.05] tracking-tight">
-                                <span className="relative inline-block">
+                                <span className="relative inline-block font-poppins font-medium mt-5">
                                     <img 
                                         src={grupodecorativo1} 
                                         alt="icono de bienvenida" 
@@ -48,7 +63,7 @@ export default function Onboarding() {
                                 <br />
 
                                 <span className="inline-flex items-center">
-                                    <span className="bg-linear-to-b from-white to-cyan-400 bg-clip-text text-transparent">K-Pulse</span>
+                                    <span className="bg-linear-to-b from-white to-cyan-300 bg-clip-text text-transparent font-poppins font-medium">K-Pulse</span>
                                     <img 
                                         src={grupodecorativo2} 
                                         alt="icono2" 
@@ -56,31 +71,30 @@ export default function Onboarding() {
                                     />
                                 </span>
                             </h1>
-                            <p className="text-white/90 mt-5 text-[17px] max-w-md leading-relaxed font-medium">
+                            <p className="font-poppins font-light text-white/90 mt-5 text-[15px] max-w-md">
                                 Transform traditional arm rehabilitation into an immersive game experience that tracks movement and progress.
                             </p>
                         </header>
 
                         <div className="flex flex-col gap-1">
                             <InfoCard 
-                                icon="?" 
+                                icon={<HelpCircle className="w-12 h-12 text-white" strokeWidth={1.9} />}  
                                 title="What is K-pulse?" 
                                 description="K-Pulse is the first 'Smart Rehab' ball designed for the modern Korean hospital." 
                             />
                             <InfoCard 
-                                icon="◎" 
+                                icon={<Target className="w-12 h-12 text-white" strokeWidth={1.7}/>}
                                 title="What can you do?" 
                                 description="K-pulse guides patients through a full upper limb rehabilitation session, combining smart ball technology and immersive minigames." 
                             />
                             <InfoCard 
-                                icon="🏆" 
+                                icon={<Trophy className="w-11 h-11 text-white" strokeWidth={1.7}/>}
                                 title="Choose your game!" 
                                 description="The smart ball controls the action on screen, guiding the user through a series of dynamic minigames powered by real movement." 
                             />
                         </div>
                     </div>
 
-                    {/* 2. Código QR (Ahora a la Derecha, Z-Index 50 para asegurar que quede al frente) */}
                     <div className="relative z-50 shrink-0 mt-35 translate-x-20">
                         <Qrcode 
                             icon={
