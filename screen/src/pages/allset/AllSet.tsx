cbb0103ed4a082e3d372020c9ba73ef962adc4a6
Mariana <1110366789@u.icesi.edu.co>
2026-04-29 21:54:05 -0500

    feat: Add last changes

--- screen/src/pages/allset/AllSet.tsx
+++ screen/src/pages/allset/AllSet.tsx
@@ -1,4 +1,4 @@
-import allsetfondo from "../../assets/allsetfondo.png"
+import allsetfondo from "../../assets/allsetfondo.png";
 
 export default function AllSet() {
     return (
@@ -6,15 +6,12 @@ export default function AllSet() {
             <img src={allsetfondo} alt="correo" className="w-full h-screen object-contain z-0 absolute" />
 
             <div className="z-10 flex w-full flex-col items-center justify-center px-6 md:px-20 lg:px-42">
-                
                 <div className="w-full max-w-2xl flex justify-center">
-                    
                     <header className="mb-6 md:mb-10 text-center font-poppins font-medium text-white text-2xl">
                         <p className="bg-linear-to-b from-white to-yellow-400 bg-clip-text text-transparent">You're all set!</p>
                         <p>enter your email to</p>
                         <p>recieve your results.</p>
                     </header>
-                    
                 </div>
             </div>
         </div>
