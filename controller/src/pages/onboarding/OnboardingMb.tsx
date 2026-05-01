f1c304ce4d4941ba53a286d886e64f846d2a6abd
Mariana <1110366789@u.icesi.edu.co>
2026-05-01 02:47:11 -0500

    feat: Add timer for screens

--- controller/src/pages/onboarding/OnboardingMb.tsx
+++ controller/src/pages/onboarding/OnboardingMb.tsx
@@ -1,24 +1,32 @@
-import fondomobile from "../../assets/fondomobile1.png"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import fondomobile from "../../assets/fondomobile1.png";
 import Qr from "./components/Qr";
 
 export default function OnboardingMb() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/onboarding2");
+        }, 10000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
     return (
         <div className="relative w-full h-screen flex flex-col items-center bg-center bg-cover bg-no-repeat overflow-hidden" style={{ backgroundImage: `url(${fondomobile})` }}>
             <div className="flex flex-col items-center justify-between w-full h-full max-w-97.5 pt-[20%] pb-[15%] px-6">
-                {/* Sección de Títulos */}
                 <div className="flex flex-col items-center text-center mt-12">
                     <h1 className="text-white text-3xl font-poppins font-medium tracking-wide">Welcome to</h1>
                     <h2 className="text-white text-4xl font-poppins font-bold mt-2">K-Pulse</h2>
-                    {/* Línea horizontal blanca debajo del título */}
                     <div className="w-48 h-1 bg-white mt-6 rounded-full"></div>
                 </div>
 
-                {/* Sección del Código QR */}
                 <div className="w-full flex justify-center my-auto">
                     <Qr />
                 </div>
 
-                {/* Sección del Texto Inferior */}
                 <div className="flex flex-col items-center text-center mb-8">
                     <p className="text-white text-2xl font-poppins font-medium">
                         Scan the <span className="bg-linear-to-b from-white to-yellow-500 bg-clip-text text-transparent font-poppins font-bold">QR</span> to
@@ -29,4 +37,3 @@ export default function OnboardingMb() {
         </div>
     );
 }
-
